cd37ac9079abb3fb2c2df63402908bb20c6b7c82
Chase Smith <marinersrock88@yahoo.com>
2017-05-23 14:48:38 -0700

    Checkpoint 22 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -24,5 +24,6 @@ window.onload = function() {
      window.addEventListener('scroll', function(event) {
          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
              animatePoints(pointsArray); 
+         }
      });
 };
